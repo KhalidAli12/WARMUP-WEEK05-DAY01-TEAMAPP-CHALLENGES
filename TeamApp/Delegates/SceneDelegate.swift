63bf406620f5d4560c1c75fe1800ba738e66d95b
Abdulaziz Alfayaa <abdulaziz.alfayaa@tuwaiq.edu.sa>
2021-10-27 18:59:14 +0300

    Remove TabBarVC custom class

--- TeamApp/Delegates/SceneDelegate.swift
+++ TeamApp/Delegates/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        let tabBarVC = window?.rootViewController as! TabBarVC
+        let tabBarVC = window?.rootViewController as! UITabBarController
         
         let T1000VC = tabBarVC.viewControllers?[0].children[0] as! T1000VC
         let tableVC = tabBarVC.viewControllers?[1].children[0] as! TableVC
